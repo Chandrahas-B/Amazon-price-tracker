e61d082d0b5b2c4b2b3571709569143fc2b71c85
Chandrahas B <372002chandrahas@gmail.com>
2023-08-20 22:22:21 +0530

    updated authentication

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.app import app
+from src.models import create_table, connection
 
 
 
 if __name__ == '__main__':
+    conn = connection()
+    create_table(conn)
     app.run(debug=True, port= 5000)
